8b6f521173964516441793b8a9dca281c047361d
manjunathak2 <manjuk20258@gmail.com>
2022-02-18 17:16:33 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import { BrowserRouter as Router, Route } from "react-router-dom";
-
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 
